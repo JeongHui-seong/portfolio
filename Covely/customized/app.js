e441eb89b07d8526663aa311fa5a7e8961e6aa3e
JeongHui-seong <134407347+JeongHui-seong@users.noreply.github.com>
2023-07-03 10:23:44 +0900

    Add files via upload

---
+++ Covely/customized/app.js
@@ -0,0 +1,11 @@
+$('#mobile-menu').on('click', function () {
+    $('.mobile-menu-con').css('z-index', 998);
+    $('.mobile-menu-con').css('opacity', 1);
+    $('.mobile-menu-wrapper').css('margin-left',0);
+})
+
+$('#mobile-menu-close').on('click', function () {
+    $('.mobile-menu-con').css('z-index', -999);
+    $('.mobile-menu-con').css('opacity', 0);
+    $('.mobile-menu-wrapper').css('margin-left','-300px');
+})
